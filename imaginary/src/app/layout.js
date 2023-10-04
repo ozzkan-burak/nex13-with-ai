4a4365b76e47827d369cee7761e7ff832025aed5
ozzkan-burak <brk.ozzkan@gmail.com>
2023-10-05 00:25:35 +0300

    episode4

--- imaginary/src/app/layout.js
+++ imaginary/src/app/layout.js
@@ -3,16 +3,11 @@ import { Inter } from 'next/font/google'
 import '@/styles/reset.css'
 import '@/styles/variables.css'
 import '@/styles/global.css'
-
-import { Inter } from 'next/font/google'
-
-const mainFont = Inter({
-  subsets: ['latin']
-})
+import { mainFont } from '@/libs/font'
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={mainFont.className}>
       <body className="layout">{children}</body>
       <div className='overlay'></div>
     </html>
