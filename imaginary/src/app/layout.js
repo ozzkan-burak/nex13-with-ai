604d2a9426852801e9411a04ceee7b9327564a60
ozzkan-burak <brk.ozzkan@gmail.com>
2023-10-05 00:08:15 +0300

    episode3

--- imaginary/src/app/layout.js
+++ imaginary/src/app/layout.js
@@ -4,6 +4,12 @@ import '@/styles/reset.css'
 import '@/styles/variables.css'
 import '@/styles/global.css'
 
+import { Inter } from 'next/font/google'
+
+const mainFont = Inter({
+  subsets: ['latin']
+})
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
